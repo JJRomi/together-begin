761490680ea87b530a8e9fd0c1320bd2a9c07060
JEONG <jromi158@gmail.com>
2024-03-07 16:28:05 +0900

    feat: setting project

---
+++ src/main/java/kr/co/togetherbegin/TogetherBeginApplication.java
@@ -0,0 +1,13 @@
+package kr.co.togetherbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherBeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherBeginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/togetherbegin/TogetherBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.togetherbegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherBeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
