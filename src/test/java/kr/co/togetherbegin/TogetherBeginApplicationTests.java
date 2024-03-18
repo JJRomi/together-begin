59a36af54f0199999877c7d28cdf26d09c77153b
JEONG <jromi158@gmail.com>
2024-03-18 17:16:58 +0900

    fix: 목표 테스트 코드 수정 - 타입 확인으로 추가 수정

--- src/test/java/kr/co/togetherbegin/TogetherBeginApplicationTests.java
+++ src/test/java/kr/co/togetherbegin/TogetherBeginApplicationTests.java
@@ -1,5 +1,9 @@
 package kr.co.togetherbegin;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.togetherbegin.presentation.dto.GoalResponseDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +11,37 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TogetherBeginApplicationTests {
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
 
     @Test
     @DisplayName("1-1. 목표 생성 - 성공")
@@ -37,17 +61,13 @@ class TogetherBeginApplicationTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(body)
         )
-        .andExpect(status().isOk())
-        .andExpect(content().json("""
-            {
-                "id": 1,
-                "title": "목표",
-                "description": "설명",
-                "startedAt": "2024-03-08",
-                "deadline": "2024-05-18",
-                "category": "목표 카테고리"
-            }
-        """));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.title").value("목표"))
+                .andExpect(jsonPath("$.description").value("설명"))
+                .andExpect(jsonPath("$.startedAt").value("2024-03-08"))
+                .andExpect(jsonPath("$.deadline").value("2024-05-18"))
+                .andExpect(jsonPath("$.category").value("목표 카테고리"));
     }
 
     @Test
@@ -81,7 +101,7 @@ class TogetherBeginApplicationTests {
     void test2_1() throws Exception {
         byte[] body = """
         {
-            "title":"목표",
+            "title":"목표3",
             "description": "설명",
             "startedAt": "2024-03-08",
             "deadline": "2024-05-18",
@@ -95,16 +115,13 @@ class TogetherBeginApplicationTests {
                                 .content(body)
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-            {
-                "id": 1,
-                "title": "목표",
-                "description": "설명",
-                "startedAt": "2024-03-08",
-                "deadline": "2024-05-18",
-                "category": "목표 카테고리"
-            }
-        """));
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("목표3"))
+                .andExpect(jsonPath("$.description").value("설명"))
+                .andExpect(jsonPath("$.startedAt").value("2024-03-08"))
+                .andExpect(jsonPath("$.deadline").value("2024-05-18"))
+                .andExpect(jsonPath("$.category").value("목표 카테고리"));
+
     }
 
     @Test
@@ -140,16 +157,12 @@ class TogetherBeginApplicationTests {
                         MockMvcRequestBuilders.get("/api/v1/goals/1")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-                    {
-                        "id": 1,
-                        "title": "목표",
-                        "description": "설명",
-                        "startedAt": "2024-03-08",
-                        "deadline": "2024-05-18",
-                        "category": "목표 카테고리"
-                    }
-                    """));
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("목표3"))
+                .andExpect(jsonPath("$.description").value("설명"))
+                .andExpect(jsonPath("$.startedAt").value("2024-03-08"))
+                .andExpect(jsonPath("$.deadline").value("2024-05-18"))
+                .andExpect(jsonPath("$.category").value("목표 카테고리"));
 
     }
 
@@ -170,22 +183,29 @@ class TogetherBeginApplicationTests {
     @Test
     @DisplayName("4-1. 목표 카테고리로 조회 - 성공")
     void test4_1() throws Exception {
-        mockMvc.perform(
+        MvcResult mvcResult = mockMvc.perform(
                         MockMvcRequestBuilders.get("/api/v1/goals?category=목표")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-                [
-                    {
-                        "id": 1,
-                        "title": "목표",
-                        "description": "설명",
-                        "startedAt": "2024-03-08",
-                        "deadline": "2024-05-18",
-                        "category": "목표 카테고리"
-                    }
-                ]
-                """));
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        List<GoalResponseDto> goalResponseDtos = objectMapper.readValue(jsonResponse, new TypeReference<List<GoalResponseDto>>() {});
+
+        assertNotNull(goalResponseDtos, "목표 리스트는 null 이 아니여야합니다.");
+        assertTrue(goalResponseDtos.size() > 0, "현재 목표 리스트는 비어있지 않아야합니다.");
+        assertFalse(goalResponseDtos.isEmpty(), "현재 목표 리스트가 비어있습니다.");
+
+        GoalResponseDto goalResponseDto = goalResponseDtos.get(0);
+
+        assertTrue(goalResponseDto.getId() instanceof Long, " 목표 아이디 타입이 잘못되었습니다.");
+        assertTrue(goalResponseDto.getTitle() instanceof String, "목표 제목 타입이 잘못되었습니다.");
+        assertTrue(goalResponseDto.getDescription() instanceof String, "목표 설명 타입이 잘못되었습니다.");
+        assertTrue(goalResponseDto.getCategory() instanceof String, "목표 카테고리 타입이 잘못되었습니다.");
+        assertTrue(goalResponseDto.getStartedAt() instanceof Date, "목표 시작일자 타입이 잘못되었습니다.");
+        assertTrue(goalResponseDto.getDeadline() instanceof Date, "목표 목표일자 타입이 잘못되었습니다.");
+
     }
 
     @Test
